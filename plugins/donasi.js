109b697092cc0d772025363cfc3ccfc435912243
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-01 19:27:07 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,12 +2,17 @@
 
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
-┌〔 Donasi • Emoney 〕
-├ GoPay: 085730903853 [AlyaXd]
-├ Dana: 082221792667 [BOTCAHX]
-├ Pulsa: 081395861695 [BOTCAHX]
-└────
-`.trim(), watermark, 'Menu', '.menu', m)
+*Allo mypren bantu donasi yuk supaya bot aktif selalu👋*
+╔═══════════════════
+║ _*DONASI UNTUK*_ BOTYNYAKAMU
+╠═══════════════════
+║╭──❉ 〔 *𝗗𝗢𝗡𝗔𝗦𝗜* 〕 ❉──────
+║│➸ *DANA* : 085828764046
+║│➸ *PULSA*: 085828764046
+║│➸ *GOPAY*: _*GAK ADA GOPAY GW MAH*_
+║╰──────────────────
+╰═════©️botynyakamu
+`.trim(), watermark, 'MENU BOT', '.menu', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
