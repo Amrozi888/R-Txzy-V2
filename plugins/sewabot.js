ec89dd690fc7473de079156ba1c26b0dcc24ec61
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-09 19:33:38 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -3,25 +3,23 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler = async(m, { conn }) => {
     let kamisato = `
 ┌〔 List Berlangganan 〕
-├ 2 Bulan
+├ 4 Bulan
 ├ 1 Bulan
 ├ 8 Bulan
-├ Trial 3 Hari
 └────
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Digi : 60149431385
-Umobile: 60189830350
+Maxis : 60147751029
+Topup: 60147751029
 `.trim()
     const button = {
         buttonText: 'List Harga',
         description: silahkan cek katalog owner https://wa.me/p/5041857039194512/62858228754046,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '2 Bulan', description: "Rp40.000\nSewa bot tanpa batasan waktu.", rowId:".masuk"},
-        {title: '1 Bulan', description: "Rp20.000\nSewa bot selama 1 bulan.", rowId:".masuk"},
-        {title: '8 Bulan', description: "Rp160.000\nSewa bot selama 8 Bulan.", rowId:".masuk"},
-        {title: 'Trial', description: "GRATIS\nBot gratis 3 Hari.", rowId:".join"},
+        {title: '4 Bulan', description: "Rm 10\nSewa bot tanpa batasan waktu.", rowId:".masuk"},
+        {title: '1 Bulan', description: "Rm 5\nSewa bot selama 1 bulan.", rowId:".masuk"},
+        {title: '8 Bulan', description: "Rm 20\nSewa bot selama 8 Bulan.", rowId:".masuk"},
         {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
         {title: 'Rules', description: "Kebijakan Privasi, Syarat Ketentuan dan Peraturan.", rowId:".snk"},
        ] }],
