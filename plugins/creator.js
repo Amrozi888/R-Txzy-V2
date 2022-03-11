746a0449c788fdb3b3fc5d1599006ea34edfea86
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-11 18:25:57 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,14 +1,19 @@
-const contacts = [
-  ['60147751029', ' owner arybotz'],
-  ['6285828764046', ' creator bot'],
-]
-function handler(m) {
-  m.reply('Ada perlu apa kak cari owner?, Mau donasi,sewa bot? Kalau enggak Jangan spam WhatsApp owner')
-  for (let [nomor, nama] of contacts) this.sendContact(m.chat, nomor + '@s.whatsapp.net', nama || conn.getName(nomor + '@s.whatsapp.net'), m)
+const { MessageType } = require('@adiwajshing/baileys')
+const PhoneNumber = require('awesome-phonenumber')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  await conn.sendMessage(m.chat, {
+    contacts: [{
+    "displayName": "ᴏᴡɴᴇʀ",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Arybotz;Owner;;;\nFN:Owner Ary Botz\nitem1.TEL;waid=60147751029:60147751029\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:noeldaniel400@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://hardianto.xyz/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇲🇾 Malaysia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Malaysia 🇲🇾\nitem5.X-ABLabel:───────[ ARYBOT ]───────\nEND:VCARD"
+  }, {
+    "displayName": "ᴏᴡɴᴇʀ",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Run;Jasa;;;\nFN:Creator\nitem1.TEL;waid=6285828764046:6285828764046\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:anidaanida59@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://hardianto.xyz/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Kalimantan Barat 🇮🇩\nitem5.X-ABLabel:───────[ JASARUN ]───────\nEND:VCARD"
+  }]
+  }, MessageType.contactsArray, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
-handler.command = /^(owner|creator)$/i
+handler.command = /^(owner)$/i
 
 module.exports = handler
