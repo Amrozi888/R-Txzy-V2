f4b639a6ffe960f7877593c6ec922b53cb9e1f11
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-03-15 20:08:57 +0700

    Update inforozi.js

--- plugins/inforozi.js
+++ plugins/inforozi.js
@@ -3,12 +3,12 @@ conn.reply(m.chat, `
 ╭─˗ˏˋ *_INFORMASI OWNER_* ´ˎ˗
 │ ✎ _Nama_ : Rozi
 │ ✎ _Umur_ : 15
-│ ✎ _Kelas_ : 9
+│ ✎ _Kelas_ : privasi
 │ ✎ _Asal_ : Pontianak
 │ ✎ _Official Grup_ :
 │    https://chat.whatsapp.com/I8Q4oJVw8buHhIgMH5iVAv
 │ ✎ _Instagram_ : 
-│    instagram.com/botynyakamu_02
+│    instagram.com/zalfapontianak
 │ ✎ _WhatsApp_ :
 │    wa.me/6285828764046
 ╰‿‿‿‿‿‿‿‿
